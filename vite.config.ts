3573b95755d3e51f85db569eb5ca75b4479eff82
Dominik Korsa <dominik.korsa@gmail.com>
2023-05-09 11:33:54 +0200

    Add dev API proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import 'dotenv/config';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+declare var process: {
+  env: Partial<Record<string, string>>;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: process.env.API_BASE_URL === undefined ? {} : {
+      '/api': {
+        target: process.env.API_BASE_URL,
+        changeOrigin: true,
+      }
+    }
+  }
 })
