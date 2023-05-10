c4b676661d4c0b740e83d0b8a36c707c75d408f3
Dominik Korsa <dominik.korsa@gmail.com>
2023-05-10 10:09:54 +0200

    Style loading view

---
+++ src/components/LoadingDots.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="loading-dots">
+    <span>.</span>
+    <span>.</span>
+    <span>.</span>
+  </span>
+</template>
+
+<style lang="scss">
+.loading-dots {
+  text-align: center;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  & > span {
+    letter-spacing: 2px;
+    animation: jump infinite 1300ms ease-in-out;
+  }
+
+  @keyframes jump {
+    0% {
+      transform: translateY(0px);
+    }
+    25% {
+      transform: translateY(-6px);
+    }
+    40% {
+      transform: translateY(2px);
+    }
+    50% {
+      transform: translateY(0);
+    }
+  }
+
+  @for $i from 1 through 3  {
+    & > span:nth-child(#{$i}) {
+      animation-delay: ($i - 1) * 300ms;
+    }
+  }
+}
+</style>
--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="loading-page">
-      LOADING
+    <h1 class="loading-page__title">Wczytywanie<loading-dots /></h1>
   </div>
 </template>
+
+<script lang="ts" setup>
+import {useHTMLClass} from "../composables/html-class";
+import LoadingDots from "../components/LoadingDots.vue";
+
+useHTMLClass('page--loading');
+</script>
+
+
+<style lang="scss">
+body:has(.loading-page), html.page--loading body {
+  height: 100vh;
+  overflow: hidden;
+
+  #app {
+    height: 100%;
+  }
+}
+
+.loading-page {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  .loading-page__title {
+    font-size: 1.5rem;
+    font-weight: normal;
+  }
+}
+</style>
